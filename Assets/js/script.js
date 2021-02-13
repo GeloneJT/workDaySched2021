d1ab0e041b179b984ae8f503da88e4e4c7ec0ce8
Jelani Thomas <jelani13@icloud.com>
2021-02-12 22:46:48 -0500

    added conditions for color change

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,25 +6,84 @@ $(".saveBtn").on("click", function () {
   localStorage.setItem(time, value);
 });
 // Adds current date and time to header
-function time() {
-  var currentTime = moment().format("MMM Do, YYYY hh:mm:ss A");
-  $("#time-display").text(currentTime);
-}
-
-setInterval(time, 1000);
+var currentTime = moment().format("MMM Do, YYYY hh:mm A");
+$("#time-display").text(currentTime);
 
 $("hour-9 .task").val(localStorage.getItem("hour-9"));
 
-
-//Assess time passed, time current; time imminent to color code in grey, red, and green respectively
 var atm = new Date().getHours();
+console.log(atm);
 
-function colorgreyRedGreen();
-    if (atm > 9){
-        $("task9").addClass("past");
-    }else if (atm >= 9 && atm < 10){
-        $("task9").addClass("presemt");
-    }else if (atm < 9) {
-        $("task9").addClass("future");
-    }
-    
+//Assess time passed vs time current vs time imminent to color code in grey(past), red(current), and green(future) respectively
+function TasksToDo() {
+  if (atm > 9) {
+    $("task9").addClass("past");
+  } else if (atm >= 9 && atm < 10) {
+    $("task9").addClass("presemt");
+  } else if (atm < 9) {
+    $("task9").addClass("future");
+  }
+
+  if (atm > 9) {
+    $("task9").addClass("past");
+  } else if (atm >= 9 && atm < 10) {
+    $("task9").addClass("presemt");
+  } else if (atm < 9) {
+    $("task9").addClass("future");
+  }
+
+  if (atm > 9) {
+    $("task9").addClass("past");
+  } else if (atm >= 9 && atm < 10) {
+    $("task9").addClass("presemt");
+  } else if (atm < 9) {
+    $("task9").addClass("future");
+  }
+}
+if (atm > 9) {
+  $("task9").addClass("past");
+} else if (atm >= 9 && atm < 10) {
+  $("task9").addClass("presemt");
+} else if (atm < 9) {
+  $("task9").addClass("future");
+}
+
+if (atm > 9) {
+  $("task9").addClass("past");
+} else if (atm >= 9 && atm < 10) {
+  $("task9").addClass("presemt");
+} else if (atm < 9) {
+  $("task9").addClass("future");
+}
+
+if (atm > 9) {
+  $("task9").addClass("past");
+} else if (atm >= 9 && atm < 10) {
+  $("task9").addClass("presemt");
+} else if (atm < 9) {
+  $("task9").addClass("future");
+}
+
+if (atm > 9) {
+  $("task9").addClass("past");
+} else if (atm >= 9 && atm < 10) {
+  $("task9").addClass("presemt");
+} else if (atm < 9) {
+  $("task9").addClass("future");
+}
+
+if (atm > 9) {
+  $("task9").addClass("past");
+} else if (atm >= 9 && atm < 10) {
+  $("task9").addClass("presemt");
+} else if (atm < 9) {
+  $("task9").addClass("future");
+}
+
+if (atm > 9) {
+  $("task9").addClass("past");
+} else if (atm >= 9 && atm < 10) {
+  $("task9").addClass("presemt");
+} else if (atm < 9) {
+  $("task9").addClass("future");
+}
